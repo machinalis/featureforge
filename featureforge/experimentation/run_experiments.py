921973579412dc5f057f006f05ef1f520c4c2989
Javier Mansilla <jmansilla@machinalis.com>
2014-04-25 15:54:51 -0300

    modified runner so its actually a module that allows easy creation of scripts, instead of an script itself

--- featureforge/experimentation/run_experiments.py
+++ featureforge/experimentation/run_experiments.py
@@ -2,31 +2,17 @@
 u"""Run all experiments defined on a json file, storing results on database.
 
 Usage:
-    run_experimentation.py <configs.json> <server> <dbname> <experiment_runner> [--dynconf=<dynconf>]
+    run_experiments.py <configs.json> <dbname> [--dbserver=<dbserver>]
 
 Options:
  -h --help              Show this screen.
  --version              Show Version.
- --dynconf=<dynconf>    Path to function that dynamically augment each config
-
-
-Where server is the uri of the mongodb server that stores the experimental
-results... typically "ip:port".
-
-Experiment runner <experiment_runner> is the python path for importing the function that
-tooks a config as arguments, executes the experiment, and returns a python dictionary
-describing the results.
-Dynamic Config <dynconf> is a python path for a callable that extends on run time each
-configuration dict. Examples of usage of this are:
-    - attaching the current git_version (with utils.get_git_info)
-    - some hash value of some dataset you are using
-    - etc
-
+ --dbserver=<dbserver>  URI of the mongodb server for storing results. Typically "ip:port" [default: localhost]
 """
 from __future__ import division
-import importlib
 import json
 import logging
+import sys
 
 from docopt import docopt
 from progress.bar import Bar
@@ -34,8 +20,8 @@ from progress.bar import Bar
 from featureforge.experimentation.stats_manager import StatsManager
 
 
-def main(opts, runner, conf_extender):
-    stats = StatsManager(db_uri=opts[u"<server>"],
+def _run(opts, runner, conf_extender):
+    stats = StatsManager(db_uri=opts[u"--dbserver"],
                          db_name=opts[u"<dbname>"])
 
     configs = json.load(open(opts[u"<configs.json>"]))
@@ -71,21 +57,11 @@ def main(opts, runner, conf_extender):
     bar.finish()
 
 
-def import_from_string(string):
-    import_path, import_name = string.rsplit('.', 1)
-    mod = importlib.import_module(import_path)
-    return getattr(mod, import_name)
-
-
-if __name__ == u"__main__":
+def main(runner, conf_extender=None, version=u'Run experiments 0.1'):
+    command_name = sys.argv[0]
+    custom__doc__ = __doc__.replace(u'run_experiments.py', command_name)
     logging.basicConfig(level=logging.DEBUG,
                         format=u"\n%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-    options = docopt(__doc__, version=u'Run configurations 0.1')
-    runner = import_from_string(options[u'<experiment_runner>'])
-    if options.get(u'--dynconf', None):
-        conf_extender = import_from_string(options[u'--dynconf'])
-    else:
-        conf_extender = None
-
-    main(options, runner, conf_extender)
+    options = docopt(custom__doc__, version=version)
+    _run(options, runner, conf_extender)
