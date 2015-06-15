73a5e376b8529b14a1beab799ba74ba1c3ca5984
Javier Mansilla <jmansilla@machinalis.com>
2015-06-15 16:45:57 -0300

    uh, ugly syntax error made by copu and paste. Fixed

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import mock
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
                 'scipy.interpolate', 'numpy.distutils.core']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 import sys, os
 
