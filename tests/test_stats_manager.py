fe841926e64a0dd9f2a228a1702bad3faac8cbfc
Julian Bigatti <jbigatti@machinalis.com>
2016-10-17 16:46:36 -0300

    mock mongo db creation

--- tests/test_stats_manager.py
+++ tests/test_stats_manager.py
@@ -1,14 +1,17 @@
 from datetime import timedelta
+import mock
 from unittest import TestCase
 import warnings
 
 from featureforge.experimentation.stats_manager import StatsManager
 
 DEPRECATION_MSG = (
-    'Init arguments will change position. '
+    'Init arguments will change. '
     'Take a look to http://feature-forge.readthedocs.io/en/latest/experimentation.html'
     '#exploring-the-finished-experiments'
 )
+
+DB_CONNECTION_PATH = 'featureforge.experimentation.stats_manager.StatsManager.setup_database_connection'   # NOQA
 
 
 class TestStatsManager(TestCase):
@@ -18,19 +21,31 @@ class TestStatsManager(TestCase):
 
     def test_init_with_db_name_as_first_parameter_and_booking_duration_as_second(self):
         booking_duration = 10
-        st = StatsManager(db_name=self.db_name, booking_duration=booking_duration)
-        self.assertEqual(st._db_config['name'], self.db_name)
-        self.assertEqual(st.booking_delta, timedelta(seconds=booking_duration))
+        with mock.patch(DB_CONNECTION_PATH):
+            st = StatsManager(db_name=self.db_name, booking_duration=booking_duration)
+            self.assertEqual(st._db_config['name'], self.db_name)
+            self.assertEqual(st.booking_delta, timedelta(seconds=booking_duration))
 
     def test_if_init_with_db_name_as_second_argument_will_warning(self):
         booking_duration = 10
-        with self.assertWarns(DeprecationWarning, msg=DEPRECATION_MSG):
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always", DeprecationWarning)
             # Trigger a warning.
-            StatsManager(booking_duration, self.db_name)
+            with mock.patch(DB_CONNECTION_PATH):
+                StatsManager(booking_duration, self.db_name)
+                # Verify some things
+                self.assertEqual(len(w), 1)
+                self.assertTrue(issubclass(w[-1].category, DeprecationWarning))
+                self.assertEqual(str(w[-1].message), DEPRECATION_MSG)
 
     def test_if_use_db_name_as_second_argument_warnings_but_can_continue(self):
         booking_duration = 10
-        with self.assertWarns(DeprecationWarning, msg=DEPRECATION_MSG):
-            st = StatsManager(booking_duration, self.db_name)
-            self.assertEqual(st._db_config['name'], self.db_name)
-            self.assertEqual(st.booking_delta, timedelta(seconds=booking_duration))
+        with warnings.catch_warnings(record=True):
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always", DeprecationWarning)
+            # Trigger a warning.
+            with mock.patch(DB_CONNECTION_PATH):
+                st = StatsManager(booking_duration, self.db_name)
+                self.assertEqual(st._db_config['name'], self.db_name)
+                self.assertEqual(st.booking_delta, timedelta(seconds=booking_duration))
